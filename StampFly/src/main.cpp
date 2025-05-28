4a8d3f9b0abe69c0487809aece99846719934275
icyuya <ic211230@edu.okinawa-ct.ac.jp>
2025-05-28 12:45:42 +0900

    a

--- StampFly/src/main.cpp
+++ StampFly/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#include <M5Unified.h>
 #include "flight_control.hpp"
 
 // VL53L0X_ADDRESS           0x29
@@ -10,13 +11,15 @@ void setup()
 {
   // init_copter();
   // delay(100);
-  USBSerial.begin(115200);
+  auto cfg = M5.config();
+  // cfg.usb_cdc = true; // Enable USB CDC Serial (if available) - Removed, not a valid member
+  M5.begin(cfg); 
   delay(2000); // モニタが接続するのを待つ
   USBSerial.println("===== Setup reached! Hello from ESP32-S3! =====");
 }
 
 void loop()
 {
-  USBSerial.println("Looping... sending test message.");
+  // USBSerial.println("Looping... sending test message.");
   loop_400Hz();
 }
